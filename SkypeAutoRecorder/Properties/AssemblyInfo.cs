2fac5b3883ae1e7ad83590905167866464a68c4f
kirmir <none@none>
2012-09-02 20:39:33 +0300

    Updated main icon and logo image. Updated About window and copyright information.

--- SkypeAutoRecorder/Properties/AssemblyInfo.cs
+++ SkypeAutoRecorder/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SkypeAutoRecorder")]
-[assembly: AssemblyCopyright("Copyright © Miroshnichenko Kirill 2012")]
+[assembly: AssemblyCopyright("Copyright © Kirill Miroshnichenko 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
