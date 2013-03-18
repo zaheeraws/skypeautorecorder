99c7fb430b896ee1a4168acdf98f2aeaf040a964
kirmir <kirmir@outlook.com>
2013-03-18 23:30:11 +0200

    Removed old files. Updated ALTER CALL commands to fix issue with silence.

--- SkypeAutoRecorder/Core/SkypeApi/SkypeCommands.cs
+++ SkypeAutoRecorder/Core/SkypeApi/SkypeCommands.cs
@@ -5,10 +5,10 @@
     /// </summary>
     internal static class SkypeCommands
     {
-        public const string StartRecordOutput = "ALTER CALL {0} SET_OUTPUT FILE=\"{1}\"";
+        public const string StartRecordOutput = "ALTER CALL {0} SET_OUTPUT SOUNDCARD=\"default\", FILE=\"{1}\"";
         public const string StartRecordInput  = "ALTER CALL {0} SET_CAPTURE_MIC FILE=\"{1}\"";
-        public const string EndRecordOutput   = "ALTER CALL {0} SET_OUTPUT SOUNDCARD=\"default\"";
-        public const string EndRecordInput    = "ALTER CALL {0} SET_CAPTURE_MIC PORT=\"356\"";
+        public const string EndRecordOutput   = "ALTER CALL {0} SET_OUTPUT SOUNDCARD=\"default\", FILE=\"\"";
+        public const string EndRecordInput    = "ALTER CALL {0} SET_CAPTURE_MIC FILE=\"\"";
         public const string GetCallerName     = "GET CALL {0} PARTNER_HANDLE";
         public const string Ping              = "PING";
     }
