9ec5a4c9c2d4b185bb10731153903a6cc82c5a13
Kyrylo M. <kirmir@outlook.com>
2015-08-03 23:14:02 +0300

    Updated project info.

--- SkypeAutoRecorder/Properties/AssemblyInfo.cs
+++ SkypeAutoRecorder/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SkypeAutoRecorder")]
+[assembly: AssemblyTitle("Skype Auto Recorder")]
 [assembly: AssemblyDescription("Automatically records Skype calls depends on settings and filters by contacts names.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SkypeAutoRecorder")]
-[assembly: AssemblyCopyright("Copyright © Kirill Miroshnichenko 2012")]
+[assembly: AssemblyProduct("Skype Auto Recorder")]
+[assembly: AssemblyCopyright("Copyright © Kyrylo Myroshnychenko 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.6.0")]
-[assembly: AssemblyFileVersion("1.3.6.0")]
+[assembly: AssemblyVersion("1.3.7.0")]
+[assembly: AssemblyFileVersion("1.3.7.0")]
